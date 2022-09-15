1a2cdbd1fe76f3e0a715fdab3b5d8fb074daf186
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2022-09-15 20:09:38 +0100

    include firebase url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -6,8 +6,9 @@ type BaseURL = string;
 
 const NGROK_URL: BaseURL = 'https://a3e0-102-89-33-130.ngrok.io'; // local test
 const HEROKU_URL: BaseURL = 'https://docsndata-zapier.herokuapp.com';
+const  FIREBASE_URL: BaseURL = 'https://europe-west2-docs-n-data.cloudfunctions.net/zapierConnector';
 
-export const BASE_URL: string = `${NGROK_URL}/api/v1`;
+export const BASE_URL: string = `${FIREBASE_URL}/api/v1`;
 
 export enum Endpoint {
     Auth = 'auth',
