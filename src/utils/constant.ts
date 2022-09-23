ca6b73371e865a1ae8ead235c3c2a08605df692a
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2022-09-23 06:00:10 +0100

    fix: remove heroku base url

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -5,7 +5,6 @@
 type BaseURL = string;
 
 const NGROK_URL: BaseURL = 'https://a3e0-102-89-33-130.ngrok.io'; // local test
-const HEROKU_URL: BaseURL = 'https://docsndata-zapier.herokuapp.com';
 const  FIREBASE_URL: BaseURL = 'https://europe-west2-docs-n-data.cloudfunctions.net/zapierConnector';
 
 export const BASE_URL: string = `${FIREBASE_URL}/api/v1`;
